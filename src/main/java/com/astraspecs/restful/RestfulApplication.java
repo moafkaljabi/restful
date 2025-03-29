cf1a5933c1668c2b9b3057b144f14da0d36f6a25
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-03-29 04:45:00 +0100

    Started restful repo

---
+++ src/main/java/com/astraspecs/restful/RestfulApplication.java
@@ -0,0 +1,16 @@
+package com.astraspecs.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApplication.class, args);
+	}
+
+//	FirstController firstController;
+//
+//	firstController.sayHello();
+}
